10210dce25fb5dabbea36cb15428eae9061f93b7
Stefan Angelov <stefanangelov@deltasource.eu>
2018-12-26 02:41:18 +0200

    initialize bot service

---
+++ bot-service/src/main/java/eu/stefanangelov/chatbot/botservice/BotServiceApplication.java
@@ -0,0 +1,14 @@
+package eu.stefanangelov.chatbot.botservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotServiceApplication.class, args);
+	}
+
+}
+
