790b893ddf5354244c175ac2fae7298b022a3bc0
Stefan Angelov <stefanangelov@deltasource.eu>
2018-12-27 14:57:09 +0200

    allow cors headers extents training data for greeting flow

--- nlu-service/server.py
+++ nlu-service/server.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
+from flask_cors import CORS
 from flask_restful import Resource, Api
 from flask_jsonpify import jsonify
 from numpy import unicode
 from snips_nlu import SnipsNLUEngine
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 api = Api(app)
 engine = SnipsNLUEngine.from_path("train")
 
@@ -17,7 +19,7 @@ class NLU(Resource):
         return jsonify(parsing)
 
 
-api.add_resource(NLU, '/analyze')
+api.add_resource(NLU, '/api/analyze')
 
 if __name__ == '__main__':
     app.run(port='5002')
