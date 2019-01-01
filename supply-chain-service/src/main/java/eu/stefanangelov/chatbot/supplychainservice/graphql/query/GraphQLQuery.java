ed474af22607d73cf17839a226168dab640a22ed
Stefan Angelov <stefanangelov@deltasource.eu>
2019-01-02 01:24:10 +0200

    extend graphQL queries

--- supply-chain-service/src/main/java/eu/stefanangelov/chatbot/supplychainservice/graphql/query/GraphQLQuery.java
+++ supply-chain-service/src/main/java/eu/stefanangelov/chatbot/supplychainservice/graphql/query/GraphQLQuery.java
@@ -1,10 +1,14 @@
 package eu.stefanangelov.chatbot.supplychainservice.graphql.query;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import eu.stefanangelov.chatbot.supplychainservice.entities.OrderStatus;
 import eu.stefanangelov.chatbot.supplychainservice.entities.SalesOrder;
 import eu.stefanangelov.chatbot.supplychainservice.repositories.SalesOrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Stefan Angelov - Delta Source Bulgaria on 29.12.18.
@@ -15,7 +19,19 @@ public class GraphQLQuery implements GraphQLQueryResolver {
 
     private final SalesOrderRepository salesOrderRepository;
 
-    public SalesOrder getSalesOrderById(Long id){
+    public SalesOrder getSaleOrderById(Long id){
         return salesOrderRepository.findById(id).orElseGet(null);
+    }
+
+    public List<SalesOrder> getSaleOrdersByOrderStatus(OrderStatus orderStatus){
+        List<SalesOrder> list = new ArrayList<>();
+        salesOrderRepository.findByOrderStatus(orderStatus).forEach(x -> list.add(x));
+        return list;
+    }
+
+    public List<SalesOrder> getSaleOrders(){
+        List<SalesOrder> list = new ArrayList<>();
+         salesOrderRepository.findAll().forEach(x -> list.add(x));
+         return list;
     }
 }
--- supply-chain-service/src/main/java/eu/stefanangelov/chatbot/supplychainservice/repositories/SalesOrderRepository.java
+++ supply-chain-service/src/main/java/eu/stefanangelov/chatbot/supplychainservice/repositories/SalesOrderRepository.java
@@ -1,12 +1,17 @@
 package eu.stefanangelov.chatbot.supplychainservice.repositories;
 
+import eu.stefanangelov.chatbot.supplychainservice.entities.OrderStatus;
 import eu.stefanangelov.chatbot.supplychainservice.entities.SalesOrder;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by Stefan Angelov - Delta Source Bulgaria on 29.12.18.
  */
 @Repository
 public interface SalesOrderRepository extends CrudRepository<SalesOrder, Long> {
+
+    List<SalesOrder> findByOrderStatus(OrderStatus orderStatus);
 }
