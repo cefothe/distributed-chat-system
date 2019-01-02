a0f098f295bfd6b0ad46c191ab37acd79ec54a5f
Stefan Angelov <stefanangelov@deltasource.eu>
2019-01-02 14:03:34 +0200

    add some more logs, remove not needed configuration and extend queries

--- supply-chain-service/src/main/java/eu/stefanangelov/chatbot/supplychainservice/graphql/query/GraphQLQuery.java
+++ supply-chain-service/src/main/java/eu/stefanangelov/chatbot/supplychainservice/graphql/query/GraphQLQuery.java
@@ -5,6 +5,7 @@ import eu.stefanangelov.chatbot.supplychainservice.entities.OrderStatus;
 import eu.stefanangelov.chatbot.supplychainservice.entities.SalesOrder;
 import eu.stefanangelov.chatbot.supplychainservice.repositories.SalesOrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -13,23 +14,27 @@ import java.util.List;
 /**
  * Created by Stefan Angelov - Delta Source Bulgaria on 29.12.18.
  */
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class GraphQLQuery implements GraphQLQueryResolver {
 
     private final SalesOrderRepository salesOrderRepository;
 
-    public SalesOrder getSaleOrderById(Long id){
+    public SalesOrder getSalesOrderById(Long id){
+        log.info("Get sales order by id {}", id);
         return salesOrderRepository.findById(id).orElseGet(null);
     }
 
-    public List<SalesOrder> getSaleOrdersByOrderStatus(OrderStatus orderStatus){
+    public List<SalesOrder> getSalesOrdersByOrderStatus(OrderStatus orderStatus){
+        log.info("Get sales order by order status {}", orderStatus);
         List<SalesOrder> list = new ArrayList<>();
         salesOrderRepository.findByOrderStatus(orderStatus).forEach(x -> list.add(x));
         return list;
     }
 
-    public List<SalesOrder> getSaleOrders(){
+    public List<SalesOrder> getSalesOrders(){
+        log.info("Get all sales orders");
         List<SalesOrder> list = new ArrayList<>();
          salesOrderRepository.findAll().forEach(x -> list.add(x));
          return list;
